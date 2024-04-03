e3a2d948700e228fb8737b186117a79a27c8828d
Matthew James Collins <modish0161@gmail.com>
2024-04-03 20:28:37 +0100

    SYNCED UP

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -10,29 +10,29 @@ const Header = () => {
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
-                <Link href="/" passHref>
-                    <a className={styles.logo}>
-                        <Image src="/Logo.webp" alt="Crypto Conduit Logo" width={100} height={100} priority />
-                    </a>
+                <Link href="/" passHref className={styles.logo}>
+
+                    <Image src="/Logo.webp" alt="Crypto Conduit Logo" width={100} height={100} priority />
+
                 </Link>
                 <ul className={styles.menu}>
                     <li>
-                        <Link href="/dashboard">
+                        <Link href="/dashboard" legacyBehavior>
                             <button className={styles.button}>Dashboard</button>
                         </Link>
                     </li>
                     <li>
-                        <Link href="/swap">
+                        <Link href="/swap" legacyBehavior>
                             <button className={styles.button}>Swap</button>
                         </Link>
                     </li>
                     <li>
-                        <Link href="/signin">
+                        <Link href="/signin" legacyBehavior>
                             <button className={styles.button}>Sign In</button>
                         </Link>
                     </li>
                     <li>
-                        <Link href="/signup">
+                        <Link href="/signup" legacyBehavior>
                             <button className={styles.button}>Sign Up</button>
                         </Link>
                     </li>
